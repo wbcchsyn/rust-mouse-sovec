f22fa67d56fcdb47402bbea4732aa73762594eb2
Yoshida Shin <wbcchsyn@gmail.com>
2020-09-02 13:17:00 +0900

    Create README.md

--- src/lib.rs
+++ src/lib.rs
@@ -30,3 +30,14 @@
 // limitations under the License.
 
 #![deny(missing_docs)]
+
+//! # mouse\_sovec
+//!
+//! `mouse\_sovec` defines `SoVec` for `mouse`, which behaves like `std::collections::Vec` .
+//!
+//! (`SoVec` stands for `Small optimized Vector` .)
+//!
+//! `SoVec` will not allocate heap if the requested memory size is small enough.
+//! Instead of heap, it uses itself as a buffer then.
+//!
+//! To avoid allocating as much as possible, the performance is better than that of `std::collections::Vec` .
