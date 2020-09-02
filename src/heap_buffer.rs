0991f8c92cde14b05b7d8fbaf97d6c0733d18729
Yoshida Shin <wbcchsyn@gmail.com>
2020-09-02 17:04:04 +0900

    Create module heap_buffer

--- src/heap_buffer.rs
+++ src/heap_buffer.rs
@@ -28,21 +28,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#![deny(missing_docs)]
-
-//! # mouse\_sovec
-//!
-//! `mouse\_sovec` defines `SoVec` for `mouse`, which behaves like `std::collections::Vec` .
-//!
-//! (`SoVec` stands for `Small optimized Vector` .)
-//!
-//! `SoVec` will not allocate heap if the requested memory size is small enough.
-//! Instead of heap, it uses itself as a buffer then.
-//!
-//! To avoid allocating as much as possible, the performance is better than that of `std::collections::Vec` .
-
-mod heap_buffer;
-
-#[cfg(test)]
-mod allocator;
